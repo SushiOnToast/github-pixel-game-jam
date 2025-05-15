b147ba41ca40e7b95f6d3d6fff558f0961e86c91
Sushi <sushbomm@gmail.com>
2025-05-15 13:57:12 +0530

    Added customisable house teleporting

--- scripts/tp_zone.gd
+++ scripts/tp_zone.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
-@export var house_id = null
-@export var environment = "neighbourhood"
+@export var to_scene: PackedScene
 
 @onready var state_manager: StateManager
 @onready var texture_rect: TextureRect = $TextureRect
@@ -9,21 +8,32 @@ extends Area2D
 
 var player_in_area = false
 
+var to_scene_path: String = ""
+var to_scene_key: String = ""
+
 func _ready() -> void:
 	texture_rect.visible = false
 	label.visible = false
+	
 	state_manager = get_tree().get_root().find_child("StateManager", true, false)
+
+	# Automatically extract path and key if a scene is assigned
+	if to_scene:
+		to_scene_path = to_scene.resource_path
+		to_scene_key = to_scene.resource_path.get_file().get_basename()
+
+	# Fallback to neighbourhood if parent is a house and no scene explicitly set
+	var parent_name = get_parent().name
+	if parent_name.begins_with("House") and to_scene_path == "":
+		to_scene_path = "res://scenes/neighbourhood.tscn"
+		to_scene_key = "Neighbourhood"
 	
 func _process(delta: float) -> void:
-	if player_in_area:
-		texture_rect.visible = true
-		label.visible = true
-	else:
-		texture_rect.visible = false
-		label.visible = false
-	
+	texture_rect.visible = player_in_area
+	label.visible = player_in_area
+
 	if player_in_area and Input.is_action_just_pressed("interact"):
-		state_manager.switch_to("res://scenes/houses/house_1.tscn", "House1")
+		state_manager.switch_to(to_scene_path, to_scene_key)
 
 func _on_body_entered(body: CharacterBody2D) -> void:
 	player_in_area = true
