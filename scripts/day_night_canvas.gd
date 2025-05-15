fd981ca93f68c26206fd6e51034ad9566312c9a7
Sushi <sushbomm@gmail.com>
2025-05-15 12:40:04 +0530

    Update day_night_canvas.gd

--- scripts/day_night_canvas.gd
+++ scripts/day_night_canvas.gd
@@ -6,6 +6,7 @@ const CYCLE_DURATION = PI/2  # One full sine wave cycle
 
 @export var gradient: GradientTexture1D
 @export var cycle_speed: float = 100.0 # Adjust this to make it slower/faster
+@export var is_night = false
 
 var time: float = 0.0
 var cycle_finished: bool = false
@@ -27,8 +28,9 @@ func _process(delta: float) -> void:
 	
 	if time >= CYCLE_DURATION:
 		cycle_finished = true
+		is_night = true	
 	
-	if all_interaction:
+	if is_night:
 		self.color = Color("#1e2237")
 		
 #func _all_neighbours_interacted():
@@ -37,6 +39,10 @@ func _all_neighbours_interacted() -> bool:
 		var c = child.get_children()[0]
 		if c == null or not c.had_interaction:
 			return false
-		else:
-			c.night = true	
+	_set_all_night()
 	return true
+	
+func _set_all_night():
+	for child in neighbours.get_children():
+		var c = child.get_children()[0]
+		c.night = true
